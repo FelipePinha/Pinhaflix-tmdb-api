80330ccf3a418ee8c68b7d0580ca32c3a7409f35
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-02-06 22:17:40 -0300

    create header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,3 +1,29 @@
+import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./header.scss";
+
 export const Header = () => {
-    return <h1>Header</h1>;
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+
+    return (
+        <header className="header-bar">
+            <div className="logo-container">
+                <strong className="logo" onClick={() => navigate("/")}>
+                    PINHAFLIX
+                </strong>
+            </div>
+            <nav className="header-right-content">
+                <a href="#" className={pathname === "/" ? "active" : ""}>
+                    Home
+                </a>
+                <a href="#" className={pathname === "/movies" ? "active" : ""}>
+                    Filmes
+                </a>
+                <a href="#" className={pathname === "/series" ? "active" : ""}>
+                    Séries
+                </a>
+            </nav>
+        </header>
+    );
 };
