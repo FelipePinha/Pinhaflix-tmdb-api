b07ffc4002fb39e991c97e5f8830c93f2358bccf
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-02-08 15:52:55 -0300

    create mobile menu

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,29 +1,51 @@
-import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { X, List } from "phosphor-react";
 import "./header.scss";
 
 export const Header = () => {
-    const { pathname } = useLocation();
-    const navigate = useNavigate();
+    const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+
+    const handleToggleMenu = (e: any) => {
+        setMobileMenuIsOpen(!mobileMenuIsOpen);
+        console.log("className 👉️", e.currentTarget.className);
+
+        if (e.currentTarget.classList.contains("menu-is-open")) {
+            console.log("Element contains class");
+        } else {
+            console.log("Element does NOT contain class");
+        }
+    };
 
     return (
         <header className="header-bar">
             <div className="logo-container">
-                <strong className="logo" onClick={() => navigate("/")}>
-                    PINHAFLIX
-                </strong>
+                <strong className="logo">PINHAFLIX</strong>
             </div>
-            <nav className="header-right-content">
-                <a href="#" className={pathname === "/" ? "active" : ""}>
-                    Home
-                </a>
-                <a href="#" className={pathname === "/movies" ? "active" : ""}>
-                    Filmes
-                </a>
-                <a href="#" className={pathname === "/series" ? "active" : ""}>
-                    Séries
-                </a>
-            </nav>
+
+            <List
+                onClick={handleToggleMenu}
+                size={32}
+                className="open-mobile-menu"
+            />
+
+            <div
+                className={`header-right-content ${
+                    mobileMenuIsOpen ? "menu-is-active" : ""
+                }`}
+            >
+                <header className="mobile-menu-header">
+                    <strong className="mobile-menu-logo">PINHAFLIX</strong>
+                    <span className="close-mobile-menu">
+                        <X onClick={handleToggleMenu} />
+                    </span>
+                </header>
+
+                <nav className="links">
+                    <a href="#">Home</a>
+                    <a href="#">Filmes</a>
+                    <a href="#">Séries</a>
+                </nav>
+            </div>
         </header>
     );
 };
