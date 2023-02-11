8464d1e698cf26a24d026ad1bfd5cf6fdfa1b2fe
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-02-10 22:41:47 -0300

    create slider

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,8 +1,11 @@
 import "./movieList.scss";
+import "swiper/scss";
+import "swiper/scss/navigation";
+
 import useApi from "../../api/useApi";
-import { Link } from "react-router-dom";
 import apiConfig from "../../api/apiConfig";
 import { MovieCard } from "../MovieCard/MovieCard";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 interface MovieListProps {
     queryName: string;
@@ -29,18 +32,44 @@ export const MovieList = (props: MovieListProps) => {
     return (
         <div className="movie-list-container">
             <ul className="movie-list">
-                {results.map((movie: apiResults) => {
-                    return (
-                        <MovieCard
-                            id={movie.id}
-                            originalTitle={
-                                movie.original_title || movie.original_name
-                            }
-                            posterPath={apiConfig.image(movie.poster_path)}
-                            key={movie.id}
-                        />
-                    );
-                })}
+                <Swiper
+                    spaceBetween={20}
+                    breakpoints={{
+                        0: {
+                            slidesPerView: 1,
+                        },
+                        300: {
+                            slidesPerView: 2,
+                        },
+                        600: {
+                            slidesPerView: 3,
+                        },
+                        1000: {
+                            slidesPerView: 4,
+                        },
+                        1200: {
+                            slidesPerView: 5,
+                        },
+                    }}
+                >
+                    {results.map((movie: apiResults) => {
+                        return (
+                            <SwiperSlide key={movie.id}>
+                                <MovieCard
+                                    id={movie.id}
+                                    originalTitle={
+                                        movie.original_title ||
+                                        movie.original_name
+                                    }
+                                    posterPath={apiConfig.image(
+                                        movie.poster_path
+                                    )}
+                                    key={movie.id}
+                                />
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
             </ul>
         </div>
     );
